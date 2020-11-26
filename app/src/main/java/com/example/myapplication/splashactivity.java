9e9473b80d37f26ca971239676899bc6ebd70f96
daniel angel <aero2-chico@hotmail.com>
2020-11-25 22:45:09 -0600

    calculadora 3

---
+++ app/src/main/java/com/example/myapplication/splashactivity.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splashactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashactivity);
+
+        init();
+    }
+
+    private void init() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(splashactivity.this, MainActivity.class));
+                finish();
+            }
+        }, 1000);
+    }
+}
